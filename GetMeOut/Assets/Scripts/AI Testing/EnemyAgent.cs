f51e5ef385d0b395ce787fb5c846673af2070629
Erikduss <erik.dusschooten@student.hku.nl>
2022-01-10 15:53:34 +0100

    Updated Enemy AI with detection, chasing, free roaming and searching

--- GetMeOut/Assets/Scripts/AI Testing/EnemyAgent.cs
+++ GetMeOut/Assets/Scripts/AI Testing/EnemyAgent.cs
@@ -1,27 +1,268 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
+//TODO:
+//Fix enemy not chasing when losing line of sight.
+//Implement the monster and remove placeholders.
+//Discuss about functionality and features.
+
 public class EnemyAgent : MonoBehaviour
 {
+    private NavMeshAgent agent;
+    private GameObject player;
+
+    //AI
+    private EnemyStates currentState = EnemyStates.IDLE;
+
+    //Wandering
     [SerializeField] private List<Transform> waypointsList = new List<Transform>();
     private int currentWaypoint = 0;
-    private NavMeshAgent agent;
+    private float roamRadius = 20;
+    [SerializeField] private Transform enemyWaypoint;
+
+    private float wanderSpeed = 2.5f;
+    
+    //Detection
+    private float maxDetectionRange = 20;
+    private float enemyFov = 45;
+
+    //Chasing
+    private float maxSearchTime = 5f;
+    private float defaultPauseTime = 2f;
+
+    private float searchTimer = 0f;
+    private float pauseTime = 0f;
+
+    private Vector3 lastSeenPosition;
+
+    private float searchRadius = 10f;
+
+    private float chaseSpeed = 3.5f;
+
+    //Audio
+    private AudioSource audioPlayer;
+    [SerializeField]private AudioClip monsterRoar;
+
+    //TESTING PURPOSES
+    [SerializeField] private List<Material> enemyMats = new List<Material>();
+    private MeshRenderer agentMeshRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         agent = this.GetComponent<NavMeshAgent>();
+        audioPlayer = this.GetComponent<AudioSource>();
+        agentMeshRenderer = this.GetComponent<MeshRenderer>();
+
+        currentState = EnemyStates.WANDERING;
+
+        agent.speed = wanderSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        if(agent.remainingDistance < 1)
+        //Pause time prevents the enemy from moving to a different action while it's in the process of a different one. (for example while playing a screaming animation)
+        if (PauseTime()) return;
+
+        Debug.Log(currentState);
+
+        //if the player is spotted, chase the player
+        if (IsPlayerSeen())
         {
-            currentWaypoint++;
-            agent.SetDestination(waypointsList[currentWaypoint].position);
+            agentMeshRenderer.material = enemyMats[3];
+            if (currentState == EnemyStates.CHASING)
+            {
+                //It's already in a chase
+                ChasePlayer();
+            }
+            else
+            {
+                //Start a chase
+                StartCoroutine(StartChase());
+                lastSeenPosition = player.transform.position;
+            }
         }
+        else
+        {
+            switch (currentState)
+            {
+                case EnemyStates.IDLE:
+                        ContinueWandering();
+                    break;
+                case EnemyStates.SEARCHING:
+                        SearchForPlayer();
+                    break;
+                case EnemyStates.CHASING:
+                        //The enemy has lost the player out of sight
+                        LostPlayer();
+                    break;
+            }
+        }
+
+        //enemy should only wander when the player is not seen and when it's not searching
+        if (currentState == EnemyStates.WANDERING && agent.remainingDistance < 1)
+        {
+            agentMeshRenderer.material = enemyMats[1];
+            FreeRoam(roamRadius);
+        }
+    }
+
+    private void LostPlayer()
+    {
+        currentState = EnemyStates.SEARCHING;
+        searchTimer = maxSearchTime;
+        SearchTimeLeft();
+        agent.SetDestination(lastSeenPosition);
+    }
+
+    private void ContinueWandering()
+    {
+        currentState = EnemyStates.WANDERING;
+        agentMeshRenderer.material = enemyMats[1];
+        FreeRoam(roamRadius);
+    }
+
+    private void SearchForPlayer()
+    {
+        if (SearchTimeLeft())
+        {
+            agentMeshRenderer.material = enemyMats[2];
+
+            if(agent.remainingDistance < 1)
+            {
+                FreeRoam(searchRadius);
+            }
+        }
+        else
+        {
+            currentState = EnemyStates.IDLE;
+            agent.speed = 0;
+            agentMeshRenderer.material = enemyMats[0];
+            pauseTime = defaultPauseTime;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        //Chase the player (set animation, movement speed, etc)
+        agent.speed = chaseSpeed;
+        agent.SetDestination(player.transform.position);
+    }
+
+    private IEnumerator StartChase()
+    {
+        //start animation, sound and timer
+        pauseTime = 4f;
+        agent.speed = 0;
+        //audioPlayer.PlayOneShot(monsterRoar);
+        yield return new WaitForSeconds(4f); //animation and sound duration
+        agent.speed = chaseSpeed;
+        currentState = EnemyStates.CHASING;
+    }
+
+    private bool PauseTime()
+    {
+        pauseTime -= Time.deltaTime;
+        if (pauseTime < 0)
+        {
+            return false;
+        }
+        agent.speed = 0;
+        return true;
+    }
+
+    private void StopMovement()
+    {
+        agent.SetDestination(this.transform.position);
+    }
+
+    private void WaypointWander()
+    {
+        currentWaypoint++;
+        agent.SetDestination(waypointsList[currentWaypoint].position);
+    }
+
+    private bool SearchTimeLeft()
+    {
+        searchTimer -= Time.deltaTime;
+        if (searchTimer < 0)
+        {
+            //stop searching (if player was not found)
+            return false;
+        }
+
+        return true;
+    }
+
+    private void FreeRoam(float rad)
+    {
+        agent.speed = wanderSpeed;
+
+        Vector3 randomDirection = Random.insideUnitSphere * rad;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, rad, 1);
+        Vector3 finalPosition = hit.position;
+        agent.destination = finalPosition;
+
+        //enemyWaypoint.transform.position = finalPosition;
+    }
+
+    // check to see if a player is within viewing range
+    private bool IsPlayerNear()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < maxDetectionRange) // if true, an Enemy is within range
+            return true;
+        else return false;
+    }
+
+    // check to see if Player is in enemy's current fov
+    private bool IsPlayerInEnemyFOV()
+    {
+        Vector3 targetDir = player.transform.position - transform.position;
+        Vector3 forward = transform.forward;
+        float angle = Vector3.Angle(targetDir, forward);
+        if (angle < enemyFov)
+            return true;
+        else return false;
+    }
+
+    // can the Enemy see the Player or is the view obstructed
+    private bool IsPlayerSeen()
+    {
+        if (!IsPlayerNear() || !IsPlayerInEnemyFOV()) return false;
+
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        RaycastHit hit;
+
+        if ((Physics.Raycast(agent.transform.position, direction, out hit, maxDetectionRange) && hit.collider.tag == "Player"))
+        {
+            Debug.Log("Sees player");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Player is behind obstacle");
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Handles.color = Color.yellow;
+        Vector3 endPointLeft = this.transform.position + (Quaternion.Euler(0, -enemyFov, 0) * this.transform.transform.forward).normalized * maxDetectionRange;
+        Vector3 endPointRight = this.transform.position + (Quaternion.Euler(0, enemyFov, 0) * this.transform.transform.forward).normalized * maxDetectionRange;
+
+        Handles.DrawWireArc(this.transform.position, Vector3.up, Quaternion.Euler(0, -enemyFov, 0) * this.transform.transform.forward, enemyFov * 2, maxDetectionRange);
+        Gizmos.DrawLine(this.transform.position, endPointLeft);
+        Gizmos.DrawLine(this.transform.position, endPointRight);
+
     }
 }
---
+++ GetMeOut/Assets/Scripts/AI Testing/PlayerTest.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    public Camera mainCam;
+    [SerializeField] private float rotationSpeed = 180f;
+    [SerializeField] private float moveSpeed = 3;
+    private CharacterController playerController;
+    private float vert = 0;
+    private float hor = 0;
+    private Vector3 moveDirection;
+
+    private float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false;
+        Move();
+        CameraRotation();
+    }
+
+    private void Move()
+    {
+        vert = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+        Vector3 forwardDirection = Vector3.Scale(new Vector3(1, 0, 1), mainCam.transform.forward);
+        Vector3 rightDirection = Vector3.Cross(Vector3.up, forwardDirection.normalized);
+        moveDirection = forwardDirection.normalized * vert + rightDirection.normalized * hor;
+        playerController.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+
+        bool isMoving = hor != 0 || vert != 0;
+    }
+
+    private void CameraRotation()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+        mainCam.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
