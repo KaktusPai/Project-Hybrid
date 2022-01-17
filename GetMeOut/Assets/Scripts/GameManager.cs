17c6693b0a3f98296df1c033a97c883d6730be9f
Erikduss <erik.dusschooten@student.hku.nl>
2022-01-17 16:10:50 +0100

    Implemented a death and survived ending function to the game manager

--- GetMeOut/Assets/Scripts/GameManager.cs
+++ GetMeOut/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,17 @@ public class GameManager : MonoBehaviour
     private GameObject[] doorObjects;
     private List<bool> closedDoors = new List<bool>();
     private EnemyAgent enemy;
+
+    //Endings UI
+    [SerializeField] private Image deathBackgroundPanel;
+    [SerializeField] private Image deathPanel;
+    [SerializeField] private TextMeshProUGUI deathText;
+
+    [SerializeField] private Image survivedBackgroundPanel;
+    [SerializeField] private Image survivedPanel;
+    [SerializeField] private TextMeshProUGUI survivedText;
+
+    private SceneController sceneManager = new SceneController();
 
     // Start is called before the first frame update
     void Start()
@@ -62,4 +75,55 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
+    public void PlayerDied()
+    {
+        deathBackgroundPanel.gameObject.SetActive(true);
+        deathPanel.gameObject.SetActive(true);
+        deathText.gameObject.SetActive(true);
+
+        StartCoroutine(FadeDeath());
+    }
+
+    public void PlayerSurvived()
+    {
+        survivedBackgroundPanel.gameObject.SetActive(true);
+        survivedPanel.gameObject.SetActive(true);
+        survivedText.gameObject.SetActive(true);
+
+        StartCoroutine(FadeSurvived());
+    }
+
+    private IEnumerator FadeSurvived()
+    {
+        // loop over 3 second
+        for (float i = 0; i <= 3; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            survivedBackgroundPanel.color = new Color(survivedBackgroundPanel.color.r, survivedBackgroundPanel.color.g, survivedBackgroundPanel.color.b, i / 3);
+            survivedPanel.color = new Color(survivedPanel.color.r, survivedPanel.color.g, survivedPanel.color.b, i / 1.5f);
+            survivedText.color = new Color(survivedText.color.r, survivedText.color.g, survivedText.color.b, i / 1.5f);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2);
+        sceneManager.loadMainScene();
+    }
+
+    private IEnumerator FadeDeath()
+    {
+        // loop over 3 second
+        for (float i = 0; i <= 3; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            deathBackgroundPanel.color = new Color(deathBackgroundPanel.color.r, deathBackgroundPanel.color.g, deathBackgroundPanel.color.b, i/3);
+            deathPanel.color = new Color(deathPanel.color.r, deathPanel.color.g, deathPanel.color.b, i / 1.5f);
+            deathText.color = new Color(deathText.color.r, deathText.color.g, deathText.color.b, i / 1.5f);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2);
+        sceneManager.loadMainScene();
+    }
+
 }
